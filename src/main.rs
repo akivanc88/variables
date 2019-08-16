b0571fc29a808e651a8350c4748367da8cd480f0
Akash <er.akashmajumder@gmail.com>
2019-08-16 16:56:00 -0700

    Added + and *

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,9 @@ fn main() {
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+    let y = 5;
+    let y = y + 1;
+    let y = y * 5;
+    println!("The value of y  is :{}",y);
+
 }
